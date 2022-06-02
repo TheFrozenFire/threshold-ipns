bbaee283f8c0bf0e492b598f811bad46706f1cea
Justin Martin <frozenfire@thefrozenfire.com>
2022-06-02 11:05:39 -0700

    Initial commit of non-working implementation

---
+++ go.mod
@@ -0,0 +1,28 @@
+module threshold-ipns
+
+go 1.16
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/coinbase/kryptology v1.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/ipfs/go-cid v0.2.0 // indirect
+	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
+	github.com/ipfs/go-ipns v0.1.2 // indirect
+	github.com/ipfs/go-log/v2 v2.5.1 // indirect
+	github.com/ipld/go-ipld-prime v0.16.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
+	github.com/libp2p/go-libp2p-core v0.16.1 // indirect
+	github.com/libp2p/go-libp2p-record v0.1.3 // indirect
+	github.com/multiformats/go-base32 v0.0.4 // indirect
+	github.com/multiformats/go-multiaddr v0.5.0 // indirect
+	github.com/multiformats/go-multicodec v0.5.0 // indirect
+	github.com/multiformats/go-multihash v0.1.0 // indirect
+	github.com/teamortix/golang-wasm/wasm v0.0.0-20210704024101-285babe68092 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+    "bytes"
+	"fmt"
+	"time"
+	"sort"
+
+	"github.com/coinbase/kryptology/pkg/ted25519/ted25519"
+	//"github.com/teamortix/golang-wasm/wasm"
+	
+	proto "github.com/gogo/protobuf/proto"
+	pb "github.com/ipfs/go-ipns/pb"
+	//ipns "github.com/ipfs/go-ipns"
+	u "github.com/ipfs/go-ipfs-util"
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/node/basic"
+	_ "github.com/ipld/go-ipld-prime/codec/dagcbor"
+	ipldcodec "github.com/ipld/go-ipld-prime/multicodec"
+	"github.com/multiformats/go-multicodec"
+	mh "github.com/multiformats/go-multihash"
+	peer "github.com/libp2p/go-libp2p-core/peer"
+)
+
+const (
+	validity     = "Validity"
+	validityType = "ValidityType"
+	value        = "Value"
+	sequence     = "Sequence"
+	ttl          = "TTL"
+)
+
+func createCborDataForIpnsEntry(e *pb.IpnsEntry) ([]byte, error) {
+	m := make(map[string]ipld.Node)
+	var keys []string
+	m[value] = basicnode.NewBytes(e.GetValue())
+	keys = append(keys, value)
+
+	m[validity] = basicnode.NewBytes(e.GetValidity())
+	keys = append(keys, validity)
+
+	m[validityType] = basicnode.NewInt(int64(e.GetValidityType()))
+	keys = append(keys, validityType)
+
+	m[sequence] = basicnode.NewInt(int64(e.GetSequence()))
+	keys = append(keys, sequence)
+
+	m[ttl] = basicnode.NewInt(int64(e.GetTtl()))
+	keys = append(keys, ttl)
+
+	sort.Sort(cborMapKeyString_RFC7049(keys))
+
+	newNd := basicnode.Prototype__Map{}.NewBuilder()
+	ma, err := newNd.BeginMap(int64(len(keys)))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, k := range keys {
+		if err := ma.AssembleKey().AssignString(k); err != nil {
+			return nil, err
+		}
+		if err := ma.AssembleValue().AssignNode(m[k]); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := ma.Finish(); err != nil {
+		return nil, err
+	}
+
+	nd := newNd.Build()
+
+	enc, err := ipldcodec.LookupEncoder(uint64(multicodec.DagCbor))
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	if err := enc(nd, buf); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type cborMapKeyString_RFC7049 []string
+
+func (x cborMapKeyString_RFC7049) Len() int      { return len(x) }
+func (x cborMapKeyString_RFC7049) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+func (x cborMapKeyString_RFC7049) Less(i, j int) bool {
+	li, lj := len(x[i]), len(x[j])
+	if li == lj {
+		return x[i] < x[j]
+	}
+	return li < lj
+}
+
+func ipnsEntryDataForSigV1(e *pb.IpnsEntry) []byte {
+	return bytes.Join([][]byte{
+		e.Value,
+		e.Validity,
+		[]byte(fmt.Sprint(e.GetValidityType())),
+	},
+		[]byte{})
+}
+
+func ipnsEntryDataForSigV2(e *pb.IpnsEntry) ([]byte, error) {
+	dataForSig := []byte("ipns-signature:")
+	dataForSig = append(dataForSig, e.Data...)
+
+	return dataForSig, nil
+}
+
+func main() {
+
+	
+	
+	entry := new(pb.IpnsEntry)
+	
+	entry.Value = []byte("/ipfs/bafybeih6ynnlxe4madt43ynit6f34dfbgjnigiggwusaiuwq3c6dmuosqi")
+	typ := pb.IpnsEntry_EOL
+	entry.ValidityType = &typ
+	seq := uint64(0)
+	entry.Sequence = &seq
+	entry.Validity = []byte(u.FormatRFC3339(time.Date(2030,time.January,1,0,0,0,0,time.UTC)))
+	
+	ttl, _ := time.ParseDuration("5s")
+	ttlNs := uint64(ttl.Nanoseconds())
+	entry.Ttl = proto.Uint64(ttlNs)
+	
+	cborData, _ := createCborDataForIpnsEntry(entry)
+	entry.Data = cborData
+	
+	messageV1 := []byte(ipnsEntryDataForSigV1(entry))
+	//messageV2 := []byte(ipnsEntryDataForSigV2(entry))
+	
+	////////
+
+	config := ted25519.ShareConfiguration{T: 2, N: 3}
+	pub, secretShares, _, _ := ted25519.GenerateSharedKey(&config)
+
+	// Each party generates a nonce and we combine them together into an aggregate one
+	noncePub1, nonceShares1, _, _ := ted25519.GenerateSharedNonce(&config, secretShares[0], pub, messageV1)
+	noncePub2, nonceShares2, _, _ := ted25519.GenerateSharedNonce(&config, secretShares[1], pub, messageV1)
+	noncePub3, nonceShares3, _, _ := ted25519.GenerateSharedNonce(&config, secretShares[2], pub, messageV1)
+
+	nonceShares := []*ted25519.NonceShare{
+		nonceShares1[0].Add(nonceShares2[0]).Add(nonceShares3[0]),
+		nonceShares1[1].Add(nonceShares2[1]).Add(nonceShares3[1]),
+		nonceShares1[2].Add(nonceShares2[2]).Add(nonceShares3[2]),
+	}
+
+	noncePub := ted25519.GeAdd(ted25519.GeAdd(noncePub1, noncePub2), noncePub3)
+
+	sig1V1 := ted25519.TSign(messageV1, secretShares[0], pub, nonceShares[0], noncePub)
+	sig2V1 := ted25519.TSign(messageV1, secretShares[1], pub, nonceShares[1], noncePub)
+	sig3V1 := ted25519.TSign(messageV1, secretShares[2], pub, nonceShares[2], noncePub)
+	
+	/*sig1V2 := ted25519.TSign(messageV2, secretShares[0], pub, nonceShares[0], noncePub)
+	sig2V2 := ted25519.TSign(messageV2, secretShares[1], pub, nonceShares[1], noncePub)
+	sig3V2 := ted25519.TSign(messageV2, secretShares[2], pub, nonceShares[2], noncePub)*/
+	
+	var alg uint64 = mh.IDENTITY
+	
+	ipnshash, _ := mh.Sum(pub.Bytes(), alg, -1)
+	ipnsid := peer.ToCid(peer.ID(ipnshash))
+	
+	fmt.Printf("Public key: %x\n", pub.Bytes())
+	fmt.Printf("IPNS Address: /ipns/%s\n", ipnsid)
+
+	fmt.Printf("\nThreshold Sig1 V1: %x\n", sig1V1.Bytes())
+	fmt.Printf("Threshold Sig2 V1: %x\n", sig2V1.Bytes())
+	fmt.Printf("Threshold Sig3 V1: %x\n\n", sig3V1.Bytes())
+
+	sigV1, _ := ted25519.Aggregate([]*ted25519.PartialSignature{sig1V1, sig3V1}, &config)
+	fmt.Printf("Rebuild signature with share 1 and 3: %x\n", sigV1)
+	
+	entry.PubKey = pub.Bytes()
+	
+	entry.SignatureV1 = sigV1
+	
+	entrydata, _ := proto.Marshal(entry)
+	fmt.Printf("Signed Entry: %x\n", entrydata)
+
+    /*
+	ok, _ := ted25519.Verify(pub, messageV1, sigV1)
+
+	if ok {
+		fmt.Printf("\nSignature verified\n\n")
+	} else {
+		fmt.Printf("\nSignature unverified\n\n")
+	}
+	*/
+	
+
+}
